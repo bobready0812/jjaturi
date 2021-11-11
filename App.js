97f9073195c33f016ffc5ac6655eb116d03d006b
DaehyeonKim <Daehyeonoff@gmail.com>
2021-11-11 13:44:40 +0900

    20211111

--- App.js
+++ App.js
@@ -61,145 +61,14 @@ const Section = ({children, title}): Node => {
 
 
 
-const Stack = createNativeStackNavigator();
-const once = true;
-//홈 화면을 보여주는 네비게이터 
-const HomeScreen = ({navigation}) => {
-
-const [doneItem, setDoneItem] = useState({}); //AsyncStorage로 부터 가져온 데이터를 Parse해서 넣어주는 state
-const [isRefreshed, setIsRefreshed] = useState(false); //새로 고침시 바뀌는 state
 
 
-const getItmes = async() => {                            //AsyncStorage로 부터 가져온 데이터를 Parse해서 doneItem에 넣어줌
-  const gotItem =await AsyncStorage.getItem('@item');
-  setDoneItem(JSON.parse(gotItem));  
-  setIsRefreshed(true);
-  
-}
-
-  
-  return(
-    <View style={styles.container}>
-    
-    <View style={styles.nav}>
-      <TouchableOpacity>
-        <Text style={styles.title}>JJATURI</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity>
-        <Text style={styles.title2}>menu</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity>
-        <Text style={styles.title2}>tag</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => {
-        navigation.navigate('addItem')
-      }}>
-        <Text style={styles.title2}>Add</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text onPress={getItmes} style={styles.title2}>Re</Text>
-      </TouchableOpacity>
-    </View>
-    <View>
-    {isRefreshed && <ScrollView>
-      {Object.keys(doneItem).map( key =><View style={styles.write} key={key}>
-           <Text style={styles.write2}>{doneItem[key].name}</Text>
-      </View>)}
-      </ScrollView>}
-      </View>
-    <View style={styles.menu}>
-      <TouchableOpacity>
-        <Text style={styles.btn}>Home</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text style={styles.btn}>Chat</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text style={styles.btn}>User</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-  )
-}
 
 
-//상품 추가하는 네비게이터
-const addItems = ({navigation}) => {
- const [name,setName] = useState("");
- const [price, setPrice] = useState("");
- const [content, setContent] = useState("");
- const sumRef = useRef(false);
- const [sum,setSum] = useState({});
- 
-//상품 명 TextInput의 값이 바뀔때마다 state를 지정해줌
-function changeName (aName) {
-  setName(aName);
-}
-//가격 TextInput의 값이 바뀔때마다 state를 지정해줌
-function changePrice (aPrice) {
-  setPrice(aPrice);
-}
-//상품 설명 TextInput의 값이 바뀔때마다 state를 지정해줌
-function changeContent (aContent) {
-  setContent(aContent);
-}
-
-//위 state들을 한 오브젝트 안에 넣어주는 함수
-const setItems = async() => {
-  
-
-  sumRef.current= true;
-  const newSum = {...sum, [Date.now()] : {name, price, content}};
-  setSum(newSum);
-  once = false;
- 
- 
 
   
   
 
-}
-
-
-//sum 의 스테이트가 업데이트 되고 난 후 AsyncStorage에 데이터를 보낸다
-useEffect(() => {
-  if(sumRef.current) {
-    // console.log(sum);
-    AsyncStorage.setItem('@item', JSON.stringify(sum));
-  }
-}, [sum]);
-
-//홈 화면으로 돌아가게 하는 함수
-const back = () => {
-  navigation.navigate('Home');
-}
-
-  return(
-   <View>
-     <View>
-      <Text>상품 이름</Text>
-      <TextInput onChangeText={changeName}></TextInput>
-     </View>
-     <View>
-      <Text>가격</Text>
-      <TextInput onChangeText={changePrice}></TextInput>
-     </View>
-     <View>
-      <Text>상품 설명</Text>
-      <TextInput multiline onChangeText={changeContent}></TextInput>
-     </View>
-     <View>
-       <Button onPress={setItems} title="완료"></Button>
-       <TouchableOpacity onPress={back}>
-        <Text>돌아가기</Text>
-       </TouchableOpacity>
-     </View>
-     
-   </View>
-  )
-}
 
 
 
@@ -216,74 +85,72 @@ const App: () => Node = () => {
 
 
   return (
-   <NavigationContainer>
-     <Stack.Navigator   screenOptions={{
-    headerShown: false
-  }}>  
-  <Stack.Screen name="Home"
-      component={HomeScreen}>     
-  </Stack.Screen> 
-  <Stack.Screen name="addItem" component={addItems}>
 
-  </Stack.Screen>
+   <View style={{flex:1}}> 
+    <View style={styles.topBar}>
 
-  </Stack.Navigator>
- 
-  </NavigationContainer> 
+    </View>
+    <View style={styles.body}>
+     <View style={styles.main}>
+       <View style={styles.title}>
+
+       </View>
+       <View style={styles.categories}>
+
+       </View>
+       <View style={styles.price}>
+
+       </View>
+       <View style={styles.content}>
+
+       </View>
+     </View>
+    </View>
+    
+  </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    fontFamily: "noto sans",
-  },
-  nav: {
-    flexDirection: "row",
-  },
-  title: {
-    marginTop: 40,
-    marginBottom: 20,
-    marginLeft: 20,
-    fontSize: 25,
-    textAlign: "left",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title2: {
-    marginTop: 40,
-    marginBottom: 20,
-    marginLeft: 20,
-    fontSize: 25,
-    color: "grey",
-    textAlign: "left",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  menu: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    marginBottom: 20,
-    marginTop: 20,
-  },
-  btn: {
-    fontSize: 23,
-  },
-  write: {
-    
-    alignItems: "center",
-    flex: 1.1,
-    color: "#787878",
-    backgroundColor: "#333333",
-    margin: 2,
-  },
-  write2: {
-    fontSize: 40,
-   color: "#787878",
-   
-   
-  },
+topBar:{
+  flex:0.06,
+  backgroundColor:"#e8dff5"
+},
+body: {
+  flex:1,
+  backgroundColor:"#daeaf6"
+},
+main:{
+  flex:1,
+  backgroundColor:"#fcf4dd",
+  marginTop:100,
+  marginBottom:50,
+  marginHorizontal:20,
+  borderRadius:13,
+ 
+},
+title: {
+ flex:1,
+ backgroundColor:"#fff", 
+ borderTopLeftRadius:13,
+ borderTopRightRadius:13,
+},
+price: {
+  flex:1,
+  backgroundColor:"black",
+},
+content : {
+  flex:3,
+  backgroundColor:"#fff",
+},
+categories: {
+  flex:1,
+}
+
+
+
+
+
 });
 
 export default App;
