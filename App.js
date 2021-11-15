35fa47c9a5c209a6cff59271f8aea15f30dd69a7
DaehyeonKim <Daehyeonoff@gmail.com>
2021-11-15 11:06:16 +0900

    20211115

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -36,7 +36,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
   
   
@@ -57,54 +57,17 @@ const App: () => Node = () => {
 
 
   return (
-   
-   <View style={{flex:1}}> 
   
-    <View style={styles.topBar}>
-       <TouchableOpacity>
-         <Text style={{fontSize:17,}}>취소</Text>
-       </TouchableOpacity>
-       <Text style={{fontSize:17}}>상품 올리기</Text>
-       <View style={{backgroundColor:"#489CCB", paddingHorizontal:8, paddingVertical:0.5, borderRadius:20}}>
-       <TouchableOpacity>
-         <Text style={{fontSize:18, color:"white" , padding:2}}>완료</Text>
-       </TouchableOpacity>
-       </View>
-       
-    </View>
-    
-    <View style={styles.stuck2}></View>
-    
+  <KeyboardAwareScrollView style={styles.body}>
+
+  </KeyboardAwareScrollView>
  
-
-    <View style={styles.body}>
-     <View style={styles.main}>
-       <View style={styles.title}>
-         <TextInput style={{fontSize:18,}} placeholder="제목"></TextInput>
-       </View>
-       <View style={styles.stuck}></View>
-       <View style={styles.categories}>
-          <Text style={{fontSize:20,}}>카테고리 설정</Text>
-       </View>
-       <View style={styles.stuck}></View>
-       <View style={styles.price}>
-          <TextInput style={{fontSize:18,}} placeholder="가격 입력  ₩"></TextInput>
-       </View>
-       <View style={styles.stuck}></View>
-       <View style={styles.content}>
-           <TextInput style={{fontSize:18,}} multiline placeholder="본문내용  입력"></TextInput>
-       </View>
-     </View>
-    </View>
-    
-
-  </View>
   );
 };
 
 const styles = StyleSheet.create({
 topBar:{
-  flex:0.06,
+  flex:1,
  
   flexDirection:"row",
   justifyContent:"space-between",
@@ -113,7 +76,7 @@ topBar:{
 },
 body: {
   flex:1,
-  backgroundColor:"#fff"
+  backgroundColor:"#FFF"
  
 },
 main:{
@@ -128,7 +91,7 @@ main:{
 },
 title: {
  flex:0.8,
-  
+ backgroundColor:"#FFF",
  borderTopLeftRadius:13,
  borderTopRightRadius:13,
  justifyContent:"center",
